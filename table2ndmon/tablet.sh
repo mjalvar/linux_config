a81536678e276e35fa9c5589d9f7c917c1f85e31
Melvin Alvarado <mjalvar@thinkpad>
2020-07-16 14:23:32 -0600

    Adding forever to tablet.

--- table2ndmon/tablet.sh
+++ table2ndmon/tablet.sh
@@ -7,7 +7,8 @@ xrandr --addmode HDMI-2 1024x600_60.00
 #xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output HDMI-2 --mode 1024x600_60.00 --pos 0x1080 --rotate normal
 xrandr --output HDMI-2 --mode "1024x600_60.00" --below eDP-1
 xhost +
-x11vnc -clip 1024x600+0+1080 -noxdamage 
+#x11vnc -clip 1024x600+0+1080 -noxdamage 
+x11vnc -clip 1024x600+0+1440 -noxdamage -localhost -forever
 
 
 # adb reverse tcp:5900 tcp:5900
