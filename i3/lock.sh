9dfa71c300876af9bdeaedf8e5266cd42df69c31
Melvin Alvarado <melvin@hpe-laptop>
2020-07-25 13:51:01 -0600

    Updating lock.sh

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ICON=$HOME/.config/i3/lock.png
+ICON=$HOME/Config/i3/lock.png
 TMPBG=/tmp/screen.png
-scrot /tmp/screen.png
+scrot -o $TMPBG
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -i $TMPBG
