5c6cbd01dedbe07770e549e81e6ede67d159e315
Melvin Alvarado <mjalvar@thinkpad>
2021-12-23 19:09:40 -0600

    Minor changes.

--- rofi/rofi.color.sh
+++ rofi/rofi.color.sh
@@ -3,7 +3,9 @@
 source "$HOME/.cache/wal/colors.sh"
 
 background='#0F0F0F'
+#background='argb:c82d303b'
 
+#                   'background', 'border', 'separator'
 rofi="rofi -combi-modi window,drun -show combi \
      -color-window '$background, $background, $color1' \
      -color-normal '$background, $foreground, $background, $color1, $background' \
