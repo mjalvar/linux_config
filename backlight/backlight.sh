6c6f52c16ea1b3b3ee231c51b064e293157a1e9a
Melvin Alvarado <mjalvar@thinkpad>
2025-08-01 19:50:07 -0600

    .

--- backlight/backlight.sh
+++ backlight/backlight.sh
@@ -3,10 +3,14 @@
 if [[ $1 == -d ]]; then
     current=$(xrandr --verbose | grep -m 1 -i brightness | awk -F' ' '{ print $2 }')
     new=$(echo "$current - 0.1" | bc -l)
+    echo $current
+    echo $new
     xrandr --output eDP-1 --brightness 0$new
 elif [[ $1 == -u ]]; then
     current=$(xrandr --verbose | grep -m 1 -i brightness | awk -F' ' '{ print $2 }')
 new=$(echo "$current + 0.1" | bc -l)
+    echo $current
+    echo $new
     xrandr --output eDP-1 --brightness 0$new
 elif [[ $1 == -m ]]; then
     xrandr --output eDP-1 --brightness 1
